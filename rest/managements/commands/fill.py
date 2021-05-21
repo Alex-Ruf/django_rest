a7af2d8de1c687ff33a8694b38d9e377291b47c3
Alex <68159105+Alex-Ruf@users.noreply.github.com>
2021-05-21 18:29:46 +0300

    2

--- rest/managements/commands/fill.py
+++
@@ -1,9 +0,0 @@
-import json
-import os
-
-from django.conf import settings
-
-
-def load_from_json(file_name):
-    with open(os.path.join(settings.BASE_DIR, f'rest/json/')) as f:
-        return json.load(f)
